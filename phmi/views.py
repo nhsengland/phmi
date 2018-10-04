dbb3eee5722bb25a76a6040c6982f67b68b28b12
George Hickman <george@ghickman.co.uk>
2018-10-04 12:17:55 +0100

    Add login form and "magic" URL logins

--- phmi/views.py
+++ phmi/views.py
@@ -1,8 +1,18 @@
+from django.contrib import messages
+from django.contrib.auth import login
 from django.shortcuts import redirect
-from django.views.generic import CreateView, DetailView, ListView, UpdateView
+from django.urls import reverse
+from django.views.generic import (
+    CreateView,
+    DetailView,
+    FormView,
+    ListView,
+    UpdateView,
+    View,
+)
 
-from .forms import CareSystemForm
-from .models import CareSystem, Organisation
+from .forms import CareSystemForm, LoginForm
+from .models import CareSystem, Organisation, User
 
 
 class GroupAdd(CreateView):
@@ -63,3 +73,41 @@ class GroupEdit(UpdateView):
 class Home(ListView):
     model = CareSystem
     template_name = "home.html"
+
+
+class Login(View):
+    def get(self, request, *args, **kwargs):
+        pk = User.get_pk_from_signed_url(kwargs["signed_pk"])
+
+        try:
+            user = User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            messages.error(request, "Unknown user, please login")
+            return redirect(reverse("login"))
+
+        # manually set the User's backend since Django's ModelBackend requires
+        # a password and we aren't setting those on our custom User model.
+        login(request, user)
+
+        # url = user.caresystem.get_absolute_url() if user.caresystem else "/"
+        url = "/"
+        return redirect(url)
+
+
+class GenerateMagicLoginURL(FormView):
+    form_class = LoginForm
+    template_name = "login.html"
+
+    def form_valid(self, form):
+        """Email a login URL to the address specified by the user."""
+        user, _ = User.objects.get_or_create(email=form.cleaned_data["email"])
+
+        url = reverse("login", kwargs={"signed_pk": user.sign_pk()})
+        absolute_url = self.request.build_absolute_uri(url)
+        user.email_login_url(absolute_url)
+
+        messages.success(
+            self.request, "A login URL has been sent tor your email address"
+        )
+
+        return redirect(reverse("request-login"))
