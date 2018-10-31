66288e237672612f4e5629dd0745677005827e1d
fredkingham <fredkingham@gmail.com>
2018-10-31 11:12:20 +0000

    puts the care system type next to the care system, reduces the care system type to the abbreviation where possible and groups by care system type on the home page refs #9

---
+++ phmi/tests/test_models.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from phmi import models
+
+
+class TestGroupType(TestCase):
+    def test_get_display_name_no_parenthesis(self):
+        self.assertEqual(
+            models.GroupType(name="hello").get_display_name(),
+            "hello"
+        )
+
+    def test_get_display_name_with_parenthesis(self):
+        self.assertEqual(
+            models.GroupType(name="hello (h)").get_display_name(),
+            "h"
+        )
