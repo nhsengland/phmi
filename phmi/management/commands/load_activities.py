3897c26ce1030db8b0b5934ca6abdedeca3fbac4
fredkingham <fredkingham@gmail.com>
2018-11-30 14:32:03 +0000

    added a favicon, changed the link colour and fixed the italic issues coming in through markdown

--- phmi/management/commands/load_activities.py
+++ phmi/management/commands/load_activities.py
@@ -8,183 +8,183 @@ IMPLIED = "Implied consent/reasonable expectations"
 SET_ASIDE = "Set aside as data will be de-identified for this purpose"
 
 
-ACT_1 = ("Allocating risk scores and stratifying populations \
+ACTIVITY_1 = ("Allocating risk scores and stratifying populations \
 for specified future adverse events causing poor health outcomes \
 to individuals", SET_ASIDE,)
 
-ACT_2 = ("Identifying and managing research cohorts", SET_ASIDE,)
+ACTIVITY_2 = ("Identifying and managing research cohorts", SET_ASIDE,)
 
-ACT_3 = (
+ACTIVITY_3 = (
     "Managing quality of health and care services (inc. clinical audit)",
     SET_ASIDE,
 )
 
-ACT_4 = ("Reviewing, evaluating and transforming current health and care \
+ACTIVITY_4 = ("Reviewing, evaluating and transforming current health and care \
 service provision across and within populations", SET_ASIDE,)
 
 
-ACT_5 = (
+ACTIVITY_5 = (
     "Systematically selecting impactible individuals within \
 risk-stratified population cohorts for early intervention or prevention",
     IMPLIED
 )
 
-ACT_6 = (
+ACTIVITY_6 = (
     "General provision of population health management", IMPLIED
 )
 
 
 
 data = {
-    ACT_1: {
+    ACTIVITY_1: {
         "Local Authority": [
-            "COMMISSIONER POWER: To provide preventative services",
+            "Commissioner Power: To provide preventative services",
         ],
         "NHS England": [
-            "DUTY: To exercise relevant public health functions",
-            "POWER: To assist SoS in providing health services and \
+            "Duty: To exercise relevant public health functions",
+            "Power: To assist SoS in providing health services and \
 exercising public health functions",
         ]
     },
-    ACT_2: {
+    ACTIVITY_2: {
         "CCG": [
-            "DUTY: To promote the involvement of each patient",
-            "DUTY: Research",
-            "DUTY: Public and patient involvement",
-            "POWER: To conduct research",
+            "Duty: To promote the involvement of each patient",
+            "Duty: Research",
+            "Duty: Public and patient involvement",
+            "Power: To conduct research",
         ],
         "NHS England": [
-            "DUTY: Public and patient involvement",
-            "POWER: To conduct research",
+            "Duty: Public and patient involvement",
+            "Power: To conduct research",
         ],
         "Local Authority": [
-            "COMMISSIONER POWER: To conduct research",
-            "COMMISSIONER DUTY:  To ensure the involvement of local people in decision-making",
+            "Commissioner Power: To conduct research",
+            "Commissioner Duty:  To ensure the involvement of local people in decision-making",
         ],
 
     },
-    ACT_3: {
+    ACTIVITY_3: {
         "CCG": [
-            "DUTY: Arrangement of personal health budgets",
+            "Duty: Arrangement of personal health budgets",
         ],
         "NHS England": [
-            "POWER: To make payments to CCGs in respect of quality of services",
-            "DUTY: Co-operation with social services regarding the provision \
+            "Power: To make payments to CCGs in respect of quality of services",
+            "Duty: Co-operation with social services regarding the provision \
 of NHS continuing healthcare",
-            "DUTY: Arrangement of personal health budgets",
+            "Duty: Arrangement of personal health budgets",
         ],
         "Local Authority": [
-            "COMMISSIONER DUTY: To make direct payments to meet care needs",
-            "COMMISSIONER DUTIES: Social care (various)",
-            "COMMISSIONER POWER: To make direct payments to patients",
+            "Commissioner Duty: To make direct payments to meet care needs",
+            "Commissioner Duties: Social care (various)",
+            "Commissioner Power: To make direct payments to patients",
         ],
     },
-    ACT_4: {
+    ACTIVITY_4: {
         "CCG": [
-            "POWER: To make direct payments to patients",
-            "DUTY: To promote the involvement of each patient",
-            "DUTY: Public and patient involvement",
-            "DUTY: To work with local authorities regarding adoption services",
-            "DUTY: To consider the economic, social and environmental benefits \
+            "Power: To make direct payments to patients",
+            "Duty: To promote the involvement of each patient",
+            "Duty: Public and patient involvement",
+            "Duty: To work with local authorities regarding adoption services",
+            "Duty: To consider the economic, social and environmental benefits \
 to be achieved through commissioning"
         ],
         "NHS England": [
-            "DUTY: Public and patient involvement",
-            "DUTY: To collect and analyse information relating to safety of \
+            "Duty: Public and patient involvement",
+            "Duty: To collect and analyse information relating to safety of \
 services",
-            "DUTY: To consider the economic, social and environmental benefits to be achieved through commissioning",
+            "Duty: To consider the economic, social and environmental benefits to be achieved through commissioning",
         ],
         "Local Authority": [
-            "COMMISSIONER DUTY: To prepare a joint strategic needs assessment",
-            "COMMISSIONER DUTY:  To ensure the involvement of local people in \
+            "Commissioner Duty: To prepare a joint strategic needs assessment",
+            "Commissioner Duty:  To ensure the involvement of local people in \
 decision-making",
-            "COMMISSIONER DUTY: To consider the economic, social and \
+            "Commissioner Duty: To consider the economic, social and \
 environmental benefits to be achieved through commissioning"
         ]
     },
-    ACT_5: {
+    ACTIVITY_5: {
         "Local Authority": [
-            "PROVIDER POWER: To provide preventative services",
-            "PROVIDER DUTIES: Social care (various)",
+            "PROVIDER Power: To provide preventative services",
+            "PROVIDER Duties: Social care (various)",
         ],
         "NHS Trust": [
-            "DUTY: Provision of NHS goods and services",
+            "Duty: Provision of NHS goods and services",
         ],
     },
-    ACT_6: {
+    ACTIVITY_6: {
         "NHS England": [
-            "DUTY: To promote a comprehensive health service",
-            "POWER:  To do anything that will facilitate the discharge of \
+            "Duty: To promote a comprehensive health service",
+            "Power:  To do anything that will facilitate the discharge of \
 functions under the 2006 Act",
-            "DUTY: To exercise functions efficiently",
-            "DUTY: To secure continuous improvement in the quality of services",
-            "DUTY:  To reduce health inequalities",
-            "DUTY: To promote integrated health services",
-            "DUTY: To promote integration between health services and \
+            "Duty: To exercise functions efficiently",
+            "Duty: To secure continuous improvement in the quality of services",
+            "Duty:  To reduce health inequalities",
+            "Duty: To promote integrated health services",
+            "Duty: To promote integration between health services and \
 health-related services",
-            "DUTY: To promote innovation",
-            "POWER: To obtain and analyse data",
-            "DUTY: Eliminate discrimination, harassment and victimisation \
+            "Duty: To promote innovation",
+            "Power: To obtain and analyse data",
+            "Duty: Eliminate discrimination, harassment and victimisation \
 and advance equality of opportunity",
-            "DUTY:  To share information",
+            "Duty:  To share information",
         ],
         "CCG": [
-            "POWER:  To do anything that will facilitate the discharge of \
+            "Power:  To do anything that will facilitate the discharge of \
 functions under the 2006 Act",
-            "POWER: To assist Secretary of State in providing health services \
+            "Power: To assist Secretary of State in providing health services \
 and exercising public health functions",
-            "DUTY:  To exercise any functions delegated to the CCG by the \
+            "Duty:  To exercise any functions delegated to the CCG by the \
 Secretary of State",
-            "DUTY: Exercise its functions efficiently",
-            "DUTY: Securing continuous improvement in quality of services \
+            "Duty: Exercise its functions efficiently",
+            "Duty: Securing continuous improvement in quality of services \
 provided to individuals",
-            "DUTY: Reduce health inequalities",
-            "DUTY: To promote innovation",
-            "DUTY: to promote integrated health services",
-            "DUTY: to promote integration between health services and \
+            "Duty: Reduce health inequalities",
+            "Duty: To promote innovation",
+            "Duty: to promote integrated health services",
+            "Duty: to promote integration between health services and \
 health-related services",
-            "POWER: For a CCG to deliver services jointly with another CCG",
-            "POWER: For a CCG to deliver services jointly with a combined \
+            "Power: For a CCG to deliver services jointly with another CCG",
+            "Power: For a CCG to deliver services jointly with a combined \
 authority",
-            "POWER: For NHS England to deliver CCG services instead of or \
+            "Power: For NHS England to deliver CCG services instead of or \
 jointly with a CCG",
-            "DUTY: To make CCG facilities available to local authorities ",
-            "POWER: To obtain and analyse data",
-            "DUTY: To co-operate with local authorities",
-            "DUTY:  To co-operate with NHS bodies when delivering care \
+            "Duty: To make CCG facilities available to local authorities ",
+            "Power: To obtain and analyse data",
+            "Duty: To co-operate with local authorities",
+            "Duty:  To co-operate with NHS bodies when delivering care \
 functions",
-            "DUTY: Eliminate discrimination, harassment and victimisation and \
+            "Duty: Eliminate discrimination, harassment and victimisation and \
 advance equality of opportunity",
-            "DUTY:  To share information",
-            "POWER: Be partnership organisations",
-            "POWER:  To partner with local authorities",
+            "Duty:  To share information",
+            "Power: Be partnership organisations",
+            "Power:  To partner with local authorities",
         ],
         "Local Authority": [
-            "DUTY:  To promote individual wellbeing",
-            "DUTY: To carry out functions with the aim of integrating services",
-            "DUTY:  To co-operate with NHS bodies when delivering care functions",
-            "DUTY:  To meet the care and support needs of eligible adults",
-            "POWER:  To meet the care and support needs of eligible adults",
-            "DUTY:  To meet the support needs of eligible carers",
-            "POWER:  To meet the support needs of eligible carers",
-            "DUTY: To improve the well-being of and reduce inequalities for children",
-            "DUTY: Eliminate discrimination, harassment and victimisation and advance equality of opportunity",
-            "DUTY:  To share information",
-            "POWER:  General power",
-            "DUTY:  To exercise any functions delegated to the local authority by the Secretary of State",
-            "DUTY:  To make services available to NHS bodies",
-            "DUTY: To co-operate with CCGs and NHS England",
-            "POWER: To obtain and analyse data",
+            "Duty:  To promote individual wellbeing",
+            "Duty: To carry out functions with the aim of integrating services",
+            "Duty:  To co-operate with NHS bodies when delivering care functions",
+            "Duty:  To meet the care and support needs of eligible adults",
+            "Power:  To meet the care and support needs of eligible adults",
+            "Duty:  To meet the support needs of eligible carers",
+            "Power:  To meet the support needs of eligible carers",
+            "Duty: To improve the well-being of and reduce inequalities for children",
+            "Duty: Eliminate discrimination, harassment and victimisation and advance equality of opportunity",
+            "Duty:  To share information",
+            "Power:  General Power",
+            "Duty:  To exercise any functions delegated to the local authority by the Secretary of State",
+            "Duty:  To make services available to NHS bodies",
+            "Duty: To co-operate with CCGs and NHS England",
+            "Power: To obtain and analyse data",
         ],
         "NHS Trust": [
-            "DUTY (NHS Trust Only): Provision of NHS goods and services",
-            "DUTY (NHS Trust Only): Efficient delivery of functions",
-            "DUTY: NHS bodies to co-operate ",
-            "POWER (NHS Trust Only): General power",
-            "POWER (NHS Trust Only): Joint exercise of functions",
-            "DUTY: Eliminate discrimination, harassment and victimisation and advance equality of opportunity",
-            "DUTY: To share information",
-            "POWER: Be partnership organisations",
+            "Duty (NHS Trust Only): Provision of NHS goods and services",
+            "Duty (NHS Trust Only): Efficient delivery of functions",
+            "Duty: NHS bodies to co-operate ",
+            "Power (NHS Trust Only): General Power",
+            "Power (NHS Trust Only): Joint exercise of functions",
+            "Duty: Eliminate discrimination, harassment and victimisation and advance equality of opportunity",
+            "Duty: To share information",
+            "Power: Be partnership organisations",
         ]
     }
 }
@@ -194,26 +194,30 @@ class Command(BaseCommand):
     help = "Load in some activities"
 
     def parse_name(self, some_name):
-        allowed_prefixes = ["DUTY", "POWER", "DUTIES"]
-        prefix, suffix = some_name.split(":", 1)
+        allowed_prefixes = ["Duty", "Power", "Duties"]
+        try:
+          prefix, suffix = some_name.split(":", 1)
+        except ValueError:
+          print(some_name)
+          raise
         for allowed_prefix in allowed_prefixes:
-            if allowed_prefix in prefix.upper():
+            if allowed_prefix in prefix:
                 return f"{allowed_prefix.strip()}: {suffix.strip()}"
-        raise ValueError(f"Unable fo parse {some_name}")
+        raise ValueError(f"Unable to parse {some_name}")
 
     def create_activities(self, activitiy, org_type, names):
         """
         Looks up for similar names in the statutes csvs.
 
         Notably the names above are more details for example they
-        have things such as COMISSIONER DUTIES rather than just
-        DUTIES as it would be called in the statutes.
+        have things such as COMISSIONER Duties rather than just
+        Duties as it would be called in the statutes.
 
         As a result we have to do some fiddling to make them
         match up.
 
         We can't just use the suffix as for some suffixes there is both
-        a DUTY and a POWER
+        a Duty and a Power
         """
         file_name = "data/csvs/activities/{}.csv".format(
             org_type.name.lower().replace(" ", "-")
@@ -255,6 +259,10 @@ class Command(BaseCommand):
 
     @transaction.atomic
     def handle(self, *args, **options):
+        self.stdout.write("Deleting all activities and legal justifications")
+        models.Activity.objects.all().delete()
+        models.LegalJustification.objects.all().delete()
+        self.stdout.write("Starting import")
         total = 0
         for activity_details, org_type_dict in data.items():
             activity, _ = models.Activity.objects.get_or_create(
