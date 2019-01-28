aa3804da298c24e9b606f1ca7b24b3ef9f7b66aa
George Hickman <george@ghickman.co.uk>
2019-01-28 12:06:40 +0000

    Remove forgotten commented out code

--- phmi/management/commands/load_justification_details.py
+++ phmi/management/commands/load_justification_details.py
@@ -15,65 +15,6 @@ class Command(BaseCommand):
             "--path", default="data/csvs/statutes", help="Path to load Statutes from"
         )
 
-    # def get_justifications(self, justification_activity, org_type_name):
-    #     """
-    #     There are some differences in terms of the prefix (e.g. commsissioner
-    #     duties vs duties) so we search by the suffix. Then do a match.
-    #     """
-    #     org_type = models.OrgType.objects.get(name=org_type_name)
-    #     prefix, justification_suffix = justification_activity.split(":", 1)
-
-    #     legal_justification = models.LegalJustification.objects.filter(
-    #         org_type=org_type, name__endswith=justification_suffix
-    #     )
-
-    #     result = []
-    #     for i in legal_justification:
-    #         if prefix.lower() in i.name.split(":")[0].lower():
-    #             result.append(i)
-
-    #     if not result:
-    #         self.stdout.write(
-    #             f"Unable to find {justification_activity} for {org_type_name}"
-    #         )
-    #         import ipdb
-
-    #         ipdb.set_trace()
-
-    #     return result
-
-    # def create_details(self, org_type_name):
-    #     """
-    #     Looks for similar names in the details csvs.
-
-    #     Notably the names of duties/powers in the statute have more details
-
-    #     for example they have things such as Comissioner duties rather than
-    #     just Duties as it would be called in the details.
-
-    #     As a result we have to do some fiddling to make them
-    #     match up.
-
-    #     We can't just use the suffix as for some suffixes there is both
-    #     a Duty and a Power
-    #     """
-    #     file_name = org_type_name.lower().replace(" ", "-")
-    #     path = f"data/csvs/statutes/{file_name}.csv"
-    #     count = 0
-
-    #     print(f"working on {path}")
-    #     with open(path) as f:
-    #         reader = csv.reader(f)
-
-    #         for row in reader:
-    #             justifications = self.get_justifications(row[1], org_type_name)
-    #             for justification in justifications:
-    #                 justification.details = row[2]
-    #                 justification.save()
-    #                 count += 1
-
-    #     return count
-
     @transaction.atomic
     def handle(self, *args, **options):
         LegalJustification.objects.all().delete()
